2ad49011a38420b4a5f39cae934753d8ddab2791
Marcin Matynia <kontakt@marcinmatynia.it>
2020-10-12 20:15:17 +0200

    Initial commit

---
+++ src/test/java/it/marcinmatynia/bookingApplication/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.marcinmatynia.bookingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
