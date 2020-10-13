8a75a3056d83ef03850b1eb21182a8db4ddfbdc2
Marcin Matynia <kontakt@marcinmatynia.it>
2020-10-13 13:08:51 +0200

    Added expert entity

--- src/main/java/it/marcinmatynia/bookingApplication/experts/Expert.java
+++ src/main/java/it/marcinmatynia/bookingApplication/experts/Expert.java
@@ -5,12 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "experts")
 class Expert {
     @Setter(AccessLevel.PACKAGE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank(message = "First name must be given")
     private String name;
+    @NotBlank(message = "Surname must be given")
     private String surname;
 }
